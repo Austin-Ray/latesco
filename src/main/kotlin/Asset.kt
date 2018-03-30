5ca63dcf777236137af9be73432fd5134d0623f0
Austin Ray <austin@austinray.io>
2018-03-29 21:15:40 -0400

    Add Asset object
    Added the most basic representation of an Asset for Latesco to grok.
    
    It includes:
    
     - A unique identifier
     - Human readable name
     - Symbol for representation
    
    The documentation includes the behavior of Latesco when initializing and
    creating a new asset. In addition, the documentation includes explicitly
    forbidden behavior of the Asset object. This will ensure the Asset class
    stays cohesive.
    
    Finally, tests were implemented to prevent regressions in the future.

---
+++ src/main/kotlin/Asset.kt
@@ -0,0 +1,20 @@
+/**
+ * Object representing an arbitrary asset in Latesco's memory.
+ *
+ * During initialization, Latesco will read the entire asset list from its database
+ * and create a list of Asset objects. This will be Latesco's working memory of how
+ * many different types of assets it currently has.
+ *
+ * When a new asset entry is created, this object will be the last piece created.
+ * The UID comes from the database so it is impossible to instantiate an object
+ * before then.
+ *
+ * This object will never store pricing information as the price could be sourced
+ * from multiple data points. The prime example is a crypto-currency's price from
+ * various exchanges.
+ *
+ * @param uid       The unique identifier the asset.
+ * @param name      The human readable name for an asset.
+ * @param symbol    The symbol which represents the asset.
+ */
+data class Asset(val uid: Long, val name: String, val symbol: String)
