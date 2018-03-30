b8d9940ae04a9a4bbc89ba8b51e3f7d49a218b9b
Austin Ray <austin@austinray.io>
2018-03-29 22:11:08 -0400

    Add PriceRecord object
    Added the PriceRecord object, which is a thin abstraction over an entry
    in the pricing table. It represents an asset's price from a specific API
    at a given time.
    
    It includes:
    
     - Asset UID
     - API UID
     - Timestamp
     - Price
    
    The BigDecimal data structure was used instead of a normal double due to
    its more granular control over arithmetic operations, rounding, and
    using base 10 representation, which makes it less likely to introduce
    error from operations.

---
+++ src/main/kotlin/PriceRecord.kt
@@ -0,0 +1,11 @@
+import java.math.BigDecimal
+
+/**
+ * Representation of an asset's price from a specific API at a given time.
+ *
+ * @param assetUid      Asset unique identifier which corresponds to this price record.
+ * @param apiUid        API unique identifier which corresponds to this price record.
+ * @param timestamp     Timestamp when the price was fetched.
+ * @param price         Price of the asset from a given API.
+ */
+data class PriceRecord(val assetUid: Long, val apiUid: Long, val timestamp: Long, val price: BigDecimal)
