4384979f0c0aba8490e5af4c5cef12376e347ce9
Austin Ray <austin@austinray.io>
2018-04-08 23:02:14 -0400

    Add PostgresConnection class
    Added a class responsible for generating the connection to the PostreSQL
    database. It handles loading the JDBC driver and URL generation.
    
    Instructions for using the JDBC driver were found on
    PostgreSQL's website[0].
    
    [0] https://jdbc.postgresql.org/documentation/head/connect.html

---
+++ src/main/kotlin/latesco/db/PostgresConnection.kt
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Latesco.
+ *
+ * Latesco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Latesco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Latesco.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package latesco.db
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.Properties
+
+private var isDriverLoaded = false
+
+/**
+ * Load the PostgreSQL JDBC driver to connect to the database.
+ */
+fun loadDriver() {
+  Class.forName("org.postgresql.Driver").newInstance()
+  isDriverLoaded = true
+}
+
+/**
+ * Connection to a PostgreSQL database using PostgreSQL's JDBC.
+ *
+ * @param host      Host's IP or URL
+ * @param db        Database name
+ * @param user      Username
+ * @param password  User's password
+ */
+class PostgresConnection(private val host: String, private val db: String, private val user: String,
+    private val password: String) {
+
+  /**
+   * Create a connection to the database with an SSL connection.
+   *
+   * @return Connection to the datatabase
+   */
+  fun getConnection() : Connection {
+    if (!isDriverLoaded) {
+      loadDriver()
+    }
+
+    val url = "jdbc:postgresql://$host/$db"
+
+    val props = Properties()
+
+    props.setProperty("user",     user)
+    props.setProperty("password", password)
+    props.setProperty("ssl",      true.toString())
+
+    return DriverManager.getConnection(url, props)
+  }
+}
